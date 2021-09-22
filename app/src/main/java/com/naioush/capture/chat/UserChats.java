a1641555480e7a2112beb81609b77edfa0f74c32
Mohanad <muhanadalkrunz@gmail.com>
2021-09-22 18:34:02 +0300

    final l

---
+++ app/src/main/java/com/naioush/capture/chat/UserChats.java
@@ -0,0 +1,25 @@
+package com.naioush.capture.chat;
+
+public class UserChats {
+
+    String user;
+
+    String chatId;
+
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getChatId() {
+        return chatId;
+    }
+
+    public void setChatId(String chatId) {
+        this.chatId = chatId;
+    }
+}
